c8a781dea14cc1211886ad0cf8a9ef3d7b4cabd2
Katharine Cooney <katharinecooney@gmail.com>
2019-06-27 12:00:55 -0500

    Refactor className for Die component

--- src/Die.js
+++ src/Die.js
@@ -2,16 +2,11 @@ import React, { Component } from 'react'
 import './Die.css';
 
 class Die extends Component {
-  
-
-
-
   render(){
-    let dieFace = this.props.dieFace;
-    let dice = `fas fa-dice-${dieFace} Die-icon`;
+    
     return(
       <div className="Die">
-        <i className={dice}></i>
+        <i className={`Die-icon fas fa-dice-${this.props.dieFace}`}></i>
       </div>
     )
   }
